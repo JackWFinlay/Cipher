a3e7835f73025aceeb4b63d2c0693b9575551023
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2015-07-29 11:37:21 +1200

    Create and update Cipher.java

---
+++ IST/Cipher/src/Cipher.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jack on 29/07/15.
+ */
+public class Cipher {
+
+    public static void main (String args[]){
+
+
+
+    }
+
+
+    private String Encrypt (String plainText){
+        String cipherText = "";
+
+
+        return cipherText;
+    }
+    
+}
