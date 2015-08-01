e53ec27f1442f220a0e8a14e9aa57ca6fa065da2
ShadowColossus <patrick.shadowcyde@gmail.com>
2015-08-01 14:48:39 +1200

    Added some console functionality, assuming a whileloop will convert all plaintext to ascii.

--- IST/Cipher/src/Cipher.java
+++ IST/Cipher/src/Cipher.java
@@ -1,15 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cipher;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 /**
- * Created by jack on 29/07/15.
+ *
+ * @author Patrick
  */
 public class Cipher {
 
-    public static void main (String args[]){
+    /**
+     * @param args the command line arguments
+     */
 
 
+    public static void main(String[] args) {
 
+        String plainText = "";
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please input your plaintext here");
+        plainText = scan.nextLine();
+
+        //This converts the first letter into ascii, maybe using a while loop will convert the whole text?
+        /*
+        char character = plainText.charAt(0);
+        int ascii = (int) character;
+
+        System.out.println(ascii);
+    }
+
+    // This worked - sort of.
+    */
+        //byte[] b = plainText.getBytes(StandardCharsets.US_ASCII);
+        //cannot pass string as byte.
+       // System.out.println((byte)plainText);
+
+        System.out.println((char)b);
     }
 
 
+    private String Decrypt(String encryptedText){
+        String decryptedText = "";
+        
+        return decryptedText;
+    }
+    
     private String Encrypt (String plainText){
         String cipherText = "";
 
